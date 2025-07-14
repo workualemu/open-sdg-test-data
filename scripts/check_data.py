b6a0a65802c8239de73459494437838b10513a17
workualemu <alemu.worku@gmail.com>
2025-07-14 16:01:07 -0400

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,9 +1,17 @@
 from sdg.open_sdg import open_sdg_check
-from sdg.inputs import InputSdmxMl_UnitedNationsApi as uapi
+# from sdg.inputs import InputSdmxMl_UnitedNationsApi as uapi
 
-uapi_object = uapi(reference_area='800')
-qur = uapi_object.get_api_query()
-print(f"QUERYYYYYYYYYY: {qur}")
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.withdraw()  # Hide the main Tkinter window
+
+messagebox.showinfo("Info", "This is a popup message!")
+
+# uapi_object = uapi(reference_area='800')
+# qur = uapi_object.get_api_query()
+# print(f"QUERYYYYYYYYYY: {qur}")
 # Validate the indicators.
 validation_successful = open_sdg_check(config='config_data.yml')
 
