fad490839febfbc765e605439219de33da7e71fd
workualemu <alemu.worku@gmail.com>
2025-07-14 16:04:16 -0400

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,13 +1,8 @@
 from sdg.open_sdg import open_sdg_check
 # from sdg.inputs import InputSdmxMl_UnitedNationsApi as uapi
 
-import tkinter as tk
-from tkinter import messagebox
-
-root = tk.Tk()
-root.withdraw()  # Hide the main Tkinter window
-
-messagebox.showinfo("Info", "This is a popup message!")
+print("Popup: Do something important now.")
+input("Press Enter to continue...")
 
 # uapi_object = uapi(reference_area='800')
 # qur = uapi_object.get_api_query()
