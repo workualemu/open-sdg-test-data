a0f3cb8817bf6da047e7912179c82629e8bfe23c
workualemu <alemu.worku@gmail.com>
2025-07-14 15:56:05 -0400

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,5 +1,9 @@
 from sdg.open_sdg import open_sdg_check
+from sdg.inputs import InputSdmxMl_UnitedNationsApi as uapi
 
+uapi_object = uapi(reference_area='800')
+qur = uapi_object.get_api_query()
+print(f"QUERYYYYYYYYYY: {qur}")
 # Validate the indicators.
 validation_successful = open_sdg_check(config='config_data.yml')
 
